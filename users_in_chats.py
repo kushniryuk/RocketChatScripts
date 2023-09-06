8b528a6b465198817eb8a345382cbe2bb6074085
kushniryuk <kusniryukigor@gmail.com>
2023-09-06 10:55:12 +0300

    Add files via upload

---
+++ users_in_chats.py
@@ -0,0 +1,49 @@
+import requests
+
+# Замените на ваши реальные данные
+auth_token = "jum51ULtevz1TA7QCp8xTZukzg9CZZNlUEEJ-Ncbv2q"
+user_id = "WDC9Fi4bxZRMGqpWb"
+rocket_chat_url = " https://techflow.cloud/"
+
+# Получите список всех чатов (комнат)
+def get_all_chats():
+    headers = {
+        "X-Auth-Token": auth_token,
+        "X-User-Id": user_id
+    }
+
+    response = requests.get(f"{rocket_chat_url}/api/v1/channels.list", headers=headers)
+
+    if response.status_code == 200:
+        return response.json().get("channels", [])
+    else:
+        print(f"Ошибка при получении списка чатов: {response.status_code}")
+        return []
+
+# Получите список пользователей в каждом чате
+def get_users_in_chat(chat_id):
+    headers = {
+        "X-Auth-Token": auth_token,
+        "X-User-Id": user_id
+    }
+
+    params = {
+        "roomId": chat_id
+    }
+
+    response = requests.get(f"{rocket_chat_url}/api/v1/channels.members", headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json().get("members", [])
+    else:
+        print(f"Ошибка при получении списка пользователей в чате {chat_id}: {response.status_code}")
+        return []
+
+# Получите список всех чатов
+all_chats = get_all_chats()
+
+# Для каждого чата, получите список пользователей
+for chat in all_chats:
+    chat_id = chat["_id"]
+    users_in_chat = get_users_in_chat(chat_id)
+    print(f"Чат: {chat['name']}, Пользователи: {', '.join([user['username'] for user in users_in_chat])}")
